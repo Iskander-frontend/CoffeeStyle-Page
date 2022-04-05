f92b12ce0d04b81ead04965959fedb7ba51c2b57
Iskander <kubeev2001@gmail.com>
2022-04-05 18:38:03 +0600

    html fix

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ let menuBtn = document.querySelector('.hamburger');
 let menu = document.querySelector('.header__menu');
 let body = document.querySelector('body');
 let product = document.querySelector('.product__price')
+let popup = document.querySelector('.popup')
+let counterBox = document.querySelector('.counter')
+let sale = document.querySelector('.sale')
+let counter = 0;
 
 menuBtn.addEventListener('click', function(){
     menuBtn.classList.toggle('active');
@@ -9,8 +13,14 @@ menuBtn.addEventListener('click', function(){
     body.classList.toggle('active');
 })
 
-product.addEventListener('click', function () {
-        console.log("penis")
-})
-
+// sale.addEventListener('click', function () {
+//     popup.classList.add('active')
+//     counter++;
+//     counterBox.textContent=counter;
+// })
+// product.addEventListener('click', function () {
+//         popup.classList.add('active')
+//         counter++;
+//         counterBox.textContent=counter;
+// })
 
